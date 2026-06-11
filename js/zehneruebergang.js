5260ce0e38be39bda73059f97f0a15f66bf846a6
lntellijent <inf25147@lehre.dhbw-stuttgart.de>
2026-06-11 16:55:40 +0200

    "Korrekt"-Nachricht hinzugefügt, angefangen die Generierungslogik zu überarbeiten

--- js/zehneruebergang.js
+++ js/zehneruebergang.js
@@ -1,38 +1,27 @@
 
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
-let generatedTask = createTask();
-
-manipulate("#leftOperand", generatedTask.leftOperand);
-manipulate("#operation", generatedTask.operation);
-manipulate("#rightOperand", generatedTask.rightOperand);
-manipulate("#relation", generatedTask.relation);
-manipulate("#result", generatedTask.result);
-// console.log(generatedTask.missing);
+let generatedTask;
+newTask();
 
 const subtn = document.querySelector("#subtn");
 const txt = document.querySelector("#userInput");
-subtn.addEventListener("click", event => {
+const helpcorrect = document.querySelector("#helpcorrectfield");
+console.log(txt);
+subtn.addEventListener("click", async event => {
     const input = Number(txt.value.trim());
     if (typeof input === "number") {
         if (input === generatedTask.missing) {
             console.log("korrekt");
-
-            generatedTask = createTask();
-
-            clear("#leftOperand");
-            clear("#operation");
-            clear("#rightOperand");
-            clear("#relation");
-            clear("#result");
-
-            manipulate("#leftOperand", generatedTask.leftOperand);
-            manipulate("#operation", generatedTask.operation);
-            manipulate("#rightOperand", generatedTask.rightOperand);
-            manipulate("#relation", generatedTask.relation);
-            manipulate("#result", generatedTask.result);
-// console.log(generatedTask.missing);
+            newTask();
+            helpcorrect.innerHTML = "Korrekt!"
+            helpcorrect.classList.toggle("hidden");
+            await delay(3000);
+            helpcorrect.classList.toggle("hidden");
         } else {
             console.log("Inkorrekt")
+            helpcorrect.innerHTML = "Inkorrekt! Versuche erneut"
+            helpcorrect.classList.toggle("hidden");
         }
     } else {
         console.log("NaN");
@@ -49,23 +38,25 @@ function clear(div) {
     list.innerHTML = "";
 }
 
-function createTask() {
-    const operation = getRandomIntInclusive(0, 1);
+function createTask(negativeEnabled) {
+    const operation = 0; // getRandomIntInclusive(0, 1);
     let leftOperand, rightOperand, result
     switch (operation) {
         case 0: // +
             leftOperand = getRandomIntInclusive(1, 89) // create the left number
-            rightOperand = getRandomIntInclusive(1, 100 - leftOperand) // create the right number - limit it to 100
+            const minimumRightValue = 10 - (leftOperand % 10);
+            rightOperand = getRandomIntInclusive(1 + minimumRightValue, 100 - leftOperand) // create the right number - limit it to 100
             result = leftOperand + rightOperand
             break;
         case 1: // -
+            // #ToDo Überarbeiten der Logik, sodass immer ein Zehnerübergang stattfindet und es einstellbar ist, ob das Ergebnis negativ werden kann oder nicht.
             leftOperand = getRandomIntInclusive(10, 100) // create the left number
             rightOperand = getRandomIntInclusive(10, 100 - leftOperand) // create the right number - limit it to 100
             result = leftOperand - rightOperand
             break;
     }
 
-    const blank = getRandomIntInclusive(0, 2)
+    const blank = 2; // getRandomIntInclusive(0, 2)
     let missing;
     switch (blank) {
         case 0:
@@ -91,6 +82,22 @@ function createTask() {
         blankIndex: blank,
         missing: missing
     };
+}
+
+function newTask() {
+    generatedTask = createTask(false);
+
+    clear("#leftOperand");
+    clear("#operation");
+    clear("#rightOperand");
+    clear("#relation");
+    clear("#result");
+
+    manipulate("#leftOperand", generatedTask.leftOperand);
+    manipulate("#operation", generatedTask.operation);
+    manipulate("#rightOperand", generatedTask.rightOperand);
+    manipulate("#relation", generatedTask.relation);
+    manipulate("#result", generatedTask.result);
 }
 
 function getRandomIntInclusive(min, max) {
